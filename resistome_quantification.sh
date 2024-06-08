8fa7a1385f8d1668ed6a1963f882e88938869419
MahmoudFaheem <60963778+MahmoudFaheem@users.noreply.github.com>
2024-06-08 23:10:39 +0300

    Update resistome_quantification.sh

--- resistome_quantification.sh
+++ resistome_quantification.sh
@@ -7,13 +7,13 @@ R2="${srr_id}_pass_2.fastq.gz"
 
 prefetch ${srr_id}
 
-#2. Convert the SRA file to Fastq format using fastq-dump. The output is generated in a fastq folder, for paired-end fastq files, R1 and R2 files will be generated.
+#2. Convert the SRA file to FASTQ format using fastq-dump. The output is generated in a fastq folder, for paired-end fastq files, R1 and R2 files will be generated.
 fastq-dump --outdir fastq --gzip --skip-technical --readids --read-filter pass --dumpbase --split-3 --clip ~/${srr_id}/${srr_id}.sra
 
-#4. Use reformat from the BBmap suite to interleave the generated forward and reverse fastq files, R_1 and R_2, and generates a single interleaved fastq file for further analysis.
+#4. Use reformat from the BBmap suite to interleave the generated forward and reverse fastq files, R_1 and R_2, and generate a single interleaved fastq file for further analysis.
 reformat.sh in1=~/fastq/${R1} in2=~/fastq/${R2} out=interleaved${srr_id}.fq.gz
 
-#5. Read Alignment with Bowtie2 to a reference database containing AMR gene sequences
+#5. Reads alignment with Bowtie2 to a reference database containing antimicrobial resistance gene sequences
 bowtie2 -x reference_index -U ~/interleaved${srr_id}.fq.gz -S ~/aligned${srr_id}.sam
 
 #6. Convert the SAM file to BAM format and sort it for downstream analysis
@@ -23,7 +23,7 @@ samtools index ~/sorted${srr_id}.bam
 #7. Convert BAM to FASTQ
 bedtools bamtofastq -i sorted${srr_id}.bam -fq sorted${srr_id}.fastq.gz
 
-#8. A single-line script to extract the gene name only and make the header tab-separated, it also creates a new column called "Gene_Count" and assigns a value of +1 to each gene every time it appears. It provides four files, diamond, awk_diamond, gene_sums [showing the number of occurrences for each unique gene], and gene_counts [which is just the total number of all genes combined]:
+#8. A single-line script to extract the gene name only and make the header tab-separated. Also, it creates a new column called "Gene_Count" and assigns a value of +1 to each gene every time it appears. It provides four files, diamond, awk_diamond, gene_sums [showing the number of occurrences for each unique gene], and gene_counts [which is just the total number of all genes combined]:
 blast_results=~/diamond_${srr_id}
 AWK_results=~/awk_diamond_${srr_id}
 
